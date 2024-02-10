47f83db7e704615f59218cab479af7c5cd8e391f
Rajat Goyal <132493320+RAJAT-COD3@users.noreply.github.com>
2024-02-11 00:38:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+import openai
 from langchain.chains.question_answering import load_qa_chain
 
 
